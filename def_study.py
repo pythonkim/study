1298aa6b5331d2e3ea272eb6412a44e5d0ec7ccd
pythonkim <godkhj@naver.com>
2019-09-01 22:45:39 +0900

    def 학습중
    앞으로 def에 관한 내용을 넣어보자~~

---
+++ def_study.py
@@ -0,0 +1,16 @@
+#기본문
+def kim():
+    print('김정남')
+
+#호출
+kim()
+
+#def의 주석, 그런데 int, str, bool 같은 것만 넣어야 한다. 진짜 주석은 아닌듯~
+#참조 https://blog.hannal.com/2015/03/keyword-only-arguments_and_annotations_for_python3/
+def moon() -> int:
+    print('문재인')
+
+#호출
+moon()
+
+
